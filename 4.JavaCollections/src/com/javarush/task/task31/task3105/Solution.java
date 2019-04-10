42b3674bdbbb7348eb52a3c99ebc2d66b988a7ab
serebryakovaN <natali.arg@mail.ru>
2019-04-10 14:23:13 +0300

    test

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3105/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task31.task3105;
+
+import java.io.IOException;
+
+/* 
+Добавление файла в архив
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        System.out.print("hdghfg");
+    }
+
+}
